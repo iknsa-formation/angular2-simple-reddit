a7e42efdf093834199250bdfd4c3698f9fb361b4
Khalid Sookia <khalidsookia@gmail.com>
2016-02-04 20:34:27 +0100

    Started app with minimum markup component

---
+++ app.ts
@@ -0,0 +1,25 @@
+import { bootstrap } from 'angular2/platform/browser';
+import { Component } from 'angular2/core';
+
+@Component({
+selector: 'reddit',
+template: `
+    <form class="ui large form segment">
+        <h3 class="ui header">Add a Link</h3>
+        <div class="field">
+            <label for ="title">Title:</label>
+            <input name="title">
+        </div>
+        <div class="field">
+            <label for ="link">Link:</label>
+            <input name="link">
+        </div>
+    </form>
+`
+})
+class RedditApp {
+    constructor() {
+    }
+}
+
+bootstrap(RedditApp);
